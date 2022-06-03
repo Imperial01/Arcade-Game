57e49637273c7dc4b4fccff5c950c51ef5e424f9
Adrian <ageimperial@gmail.com>
2022-06-03 00:09:32 -0700

    added computer player on cell element

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let gameState = {
     player: "X",
     board: [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
     ],
     changeTurn: function changeTurn() {
         gameState.player = gameState.player === "X" ? "O" : "X";
     },
     move: function () {
-        gameState.board = gameState.changeTurn;
+        gameState.board = gameState.changeTurn
     }
-};
+}
 //-------DOM
 let titleElem = document.getElementById('title')
 let board = document.querySelector('#board-container');
@@ -20,12 +20,20 @@ let form = document.getElementById('player-form');
 let playerElem = document.getElementById('player-nav')
 let player1 = document.getElementById('player1');
 let player2 = document.getElementById('player2');
-console.log(player2.value)
 let reset = document.querySelector('#reset')
+let sizing = document.getElementById('sizing-container')
+let plusBoard = document.getElementById('increase-board')
+let minusBoard = document.getElementById('decrease-board')
+console.log(sizing)
+console.log(plusBoard)
+console.log(minusBoard)
+
 
 //-------RENDER FUNCTION
 let gameArr = gameState.board;
+let mergedBoard = [].concat.apply([],gameArr)
 function renderBoard() {
+    board.innerText = null
     let count = 0
     for (let i = 0; i < gameArr.length; i++) {
         for (let j = 0; j < gameArr[i].length; j++) {
@@ -37,22 +45,16 @@ function renderBoard() {
             count++;
             grid.className = 'grid-cell';
             board.appendChild(grid);
+            
         }
-
+        
     }
-
+    
 }
 renderBoard();
+console.log(mergedBoard)
 
-let cells = document.querySelectorAll('.grid-cell');
-let boardState = Array(cells.length);
-for (let i = 0; i < boardState.length; i++) {
-    console.log(boardState[i]);
-
-}
-boardState.fill(null);
-console.log(typeof (boardState));
-
+let cells = document.querySelectorAll('.grid-cell'); //keep
 let winningCombos = [
     { combo: [0, 1, 2] },
     { combo: [3, 4, 5] },
@@ -69,84 +71,77 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     let playerRow = document.createElement('div')
     titleElem.appendChild(playerRow);
-    playerRow.className = "playerList";
-    playerElem.innerHTML = player1.value + " VERSUS " + player2.value;
-    form.style.display = "none";
-    board.dataset.play = "ON";
-
+    playerRow.className = "playerList"
+    playerElem.innerHTML = player1.value + " VERSUS " + player2.value
+    form.style.display = "none"
+    board.dataset.play = "ON"
+    console.log(player1.value)
 
     titleElem.innerHTML = player1.value + " is " + gameState.player
     if (player1 && player2 !== true) {
         reset.removeAttribute('hidden');
     }
-
-});
-
+})
+let randomNum = Math.floor(Math.random() * 9);
+let randomTurn = mergedBoard[Math.floor(Math.random() * mergedBoard.length)];
+console.log(randomNum)
 cells.forEach((cell) => cell.addEventListener('click', titleClick));
 function titleClick(event) {
-    //console.log(event.target);
+    randomNum
+    cells[randomNum];
+    console.log(event.target);
     let index = event.target;
     let indexNum = index.dataset.idx;
-    //console.log(indexNum)
-
-
+    //renderBoard();
     if (board.dataset.play !== "ON") {
         return
     }
-    if (boardState[indexNum] !== null) {
-        return;
+    if (mergedBoard[indexNum] !== null) {
+        return
     }
-    if (gameState.player == "X" && boardState[indexNum] == null) {
+    if (gameState.player == "X" && mergedBoard[indexNum] == null) {
         index.innerText = gameState.player;
-        boardState[indexNum] = gameState.player;
+        mergedBoard[indexNum] = gameState.player;
         gameState.changeTurn();
-        console.log(boardState);
+        console.log(mergedBoard);
     } else {
         index.innerText = gameState.player;
-        boardState[indexNum] = gameState.player;
+        mergedBoard[indexNum] = gameState.player;
         gameState.changeTurn();
-        //console.log(boardState[indexNum]);
+        console.log(mergedBoard[indexNum]);
     }
-    if (board.dataset.play == "ON" && player2.value == "Computer" && boardState.includes(null)) {
-        autoSelect();
-        gameState.changeTurn();
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].innerHTML = '';
-            
-        }
-        //let aiPlayer = autoSelect();
-        //console.log(boardState.indexOf(autoSelect()))
-        //let cellElemArr = Array.from(cells)
-        //console.log(cellElemArr)
-
-        console.log(boardState)
-        // cells.innerText = aiPlayer
-        // console.log(boardState)
-        // gameState.changeTurn();
-        // console.log(boardState)
-        
-    }
-
     checkWinner();
-    cells.forEach((cell) => cell.addEventListener('click', titleClick));
+    
+    if (board.dataset.play == "ON" && player2.value == "Computer" && mergedBoard.includes(null)) {
+        for (let i = 0; i < cells.length; i++){
+            //console.log(cells[i])
+            autoSelect()
+            cells[randomNum].innerHTML = "O";
+            randomNum;
+        }
+        
+        //renderBoard();
+        
+        console.log(mergedBoard)
+    }
 
+    cells.forEach((cell) => cell.addEventListener('click', titleClick));
     if (titleElem.dataset.winner === gameState.player) {
         cells.forEach((cell) => cell.removeEventListener('click', titleClick));
         reset.removeAttribute('hidden');
-        
+
     }
-    gameState.changeTurn();
-    console.log(boardState);
+    
 
 }
 
 function checkWinner() {
     for (let winningCombo of winningCombos) {
         let { combo } = winningCombo
-        let winA = boardState[combo[0]];
-        let winB = boardState[combo[1]];
-        let winC = boardState[combo[2]];
-        if (!boardState.includes(null) && titleElem.innerHTML === 'Tic-Tac-Toe') {
+        let winA = mergedBoard[combo[0]];
+        let winB = mergedBoard[combo[1]];
+        let winC = mergedBoard[combo[2]];
+        if (!mergedBoard.includes(null) && titleElem.innerHTML === 'Tic-Tac-Toe') {
             reset.removeAttribute('hidden');
             titleElem.innerHTML = ('Draw!');
             reset.innerHTML = "Play again?";
@@ -166,28 +161,29 @@ function checkWinner() {
 
 function resetButton() {
     gameState.changeTurn();
-    titleElem.innerHTML = 'Tic-Tac-Toe';
-    titleElem.dataset.winner = "";
+    titleElem.innerHTML = 'Tic-Tac-Toe'
+    titleElem.dataset.winner = ""
     cells.forEach((cell) => cell.addEventListener('click', titleClick));
-    Array(9);
-    boardState.fill(null);
-    form.style.display = 'flex';
+    mergedBoard.fill(null)
+    form.style.display = 'flex'
     for (let i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = '';
-    };
-};
+        cells[i].innerHTML = ''
+    }
+    checkWinner();
+}
 
 function autoSelect() {
-    let randomNum = Math.floor(Math.random() * 9);
-    let randomTurn = boardState[Math.floor(Math.random() * boardState.length)];
-    randomTurn = randomNum;
-    for (let i = 0; i < boardState.length; i++) {
-        boardState[randomTurn] = gameState.player;
+    console.log(randomTurn)
+    for (let i = 0; i < mergedBoard.length; i++) {
+        mergedBoard[randomNum] = "O";
     };
 
 };
-
-
-
 //bug: press start and if its a draw --> no buttons
-
+// let cellGrid = document.querySelectorAll('.grid-cell')
+// minusBoard.addEventListener('click', function (e) {
+//     board.style.height = "41vh"
+//     board.style.width = "50em"
+//     cellGrid.style.width = "15vh"
+//     cellGrid.style.height = '12vh'
+// })
